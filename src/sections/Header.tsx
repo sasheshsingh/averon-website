f9658a1542f6b31c631abf88d99557ed5ac6ff9a
Sashesh Singh <sashesh.sss@gmail.com>
2025-01-27 00:45:12 +0530

    add header

---
+++ src/sections/Header.tsx
@@ -0,0 +1,26 @@
+import LogoIcon from "@/assets/logo.svg";
+import MenuIcon from "@/assets/icon-menu.svg";
+export const Header = () => {
+  return <header className="py-4 border-b border-white/15 ">
+    <div className="container">
+      <div className="flex justify-between items-center">
+        <div>
+          <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
+            <LogoIcon className="h-8 w-8"/>
+          </div>
+        </div>
+        <div>
+          <nav className="flex gap-8 text-sm">
+            <a href="#" className=" text-gray-400 font-medium hover:text-white transition">Skyline</a>
+            <a href="#" className="text-gray-400 font-medium hover:text-white transition">Skyline</a>
+            <a href="#" className=" text-gray-400 font-medium hover:text-white transition">Skyline</a>
+            <a href="#" className=" text-gray-400 font-medium hover:text-white transition">White</a>
+          </nav>
+        </div>
+        <div className="flex gap-4 items-center">
+          <button className="border py-2 px-3 rounded-lg font-medium text-sm">Join Waitlist</button>
+        </div>
+      </div>
+    </div>
+  </header>;
+};
---
+++ src/sections/Testimonials.tsx
@@ -0,0 +1,35 @@
+import avatar1 from "@/assets/avatar-1.png";
+import avatar2 from "@/assets/avatar-2.png";
+import avatar3 from "@/assets/avatar-3.png";
+import avatar4 from "@/assets/avatar-4.png";
+
+const testimonials = [
+  {
+    text: "“This product has completely transformed how I manage my projects and deadlines”",
+    name: "Sashesh Singh",
+    title: "Averon Research",
+    avatarImg: avatar1,
+  },
+  {
+    text: "“These AI tools have completely revolutionized our SEO entire strategy overnight”",
+    name: "Sashesh Singh",
+    title: "Averon Research",
+    avatarImg: avatar2,
+  },
+  {
+    text: "“The user interface is so intuitive and easy to use, it has saved us countless hours”",
+    name: "Sashesh Singhr",
+    title: "Averon Research",
+    avatarImg: avatar3,
+  },
+  {
+    text: "“Our team's productivity has increased significantly since we started using this tool”",
+    name: "Sashesh Singh",
+    title: "Averon Research",
+    avatarImg: avatar4,
+  },
+];
+
+export const Testimonials = () => {
+  return <section>Testimonials</section>;
+};
