11a2f2f7c7c4769f3263f3be8e159943663b7b0e
Sashesh Singh <sashesh.sss@gmail.com>
2025-01-28 02:53:43 +0530

    add speed analysis package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {twMerge} from "tailwind-merge";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
