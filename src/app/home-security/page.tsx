281d3208064c1983e60a4e8ea4f00b028e0e0f5a
Sashesh Singh <sashesh.sss@gmail.com>
2025-02-02 22:10:55 +0530

    fix: update image

--- src/app/home-security/page.tsx
+++ src/app/home-security/page.tsx
@@ -21,7 +21,7 @@ export default function Main(){
             />
                 <StylishCard />
                 <BoardingImage
-                image="https://media.istockphoto.com/id/492683865/photo/drone-white-cloudy-dusk-sky.jpg?s=1024x1024&w=is&k=20&c=a3YfOSsDRyyr53JuDP8bKjmbNayvyjGZBVEoN8qHoig="
+                image="https://img.freepik.com/free-photo/3d-drone-flying-sunset-sea_1048-12782.jpg?t=st=1738013602~exp=1738017202~hmac=de8234f02684561e26aadc5773da041d3b48cb22a060e851dc9a87b44f8c50e5&w=1380"
                 title={undefined}
                 paragraph={undefined}
             />
