3dacaae6c2cb7d86a53d03e6bd8e53d384d8033b
Alisina <99441567+imalisina@users.noreply.github.com>
2025-02-05 19:20:06 +0100

    footer - done

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,126 +1,213 @@
 import React from "react";
 import {
   homeFooter,
-  productsFooter,
   skylineFooter,
   airpalFooter,
   teamFooter,
   contactFooter,
+  termsOfUseFooter,
+  faqFooter,
+  crunchbaseURL,
+  linkedinURL,
+  instagramURL,
+  footerTitleOne,
+  footerTitleTwo,
+  footerTitleThree,
+  footerTitleFour,
+  homeLink,
+  skylineLink,
+  airpalLink,
+  teamLink,
+  contactLink,
+  faqLink,
+  privacyPolicyFooter,
+  dataProtectionFooter,
+  termsOfUseLink,
+  privacyPolicyLink,
+  ethicsFooter,
+  ethicsLink,
+  dataProtectionLink,
+  instagramFooter,
+  crunchbaseFooter,
+  linkedinFooter,
+  averonURL,
+  madeWithText,
+  inSwitzerlandText,
+  copyrightSign,
+  averonTrademarkText,
+  rightsReservedText,
 } from "@/data/content";
+import Link from "next/link";
+import { Heart } from "lucide-react";
 
 const Footer = () => {
   return (
     <div className="bg-background w-full max-w-screen-xl mx-auto py-2 px-16">
       <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
+        {/* Company section */}
         <div>
-          <h2 className="mb-6 text-sm font-semibold uppercase">Home</h2>
-          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+          <h2 className="mb-6 text-primary text-sm font-semibold uppercase">
+            {footerTitleOne}
+          </h2>
+          <ul className="text-secondary font-medium">
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                About
-              </a>
+              <Link
+                href={homeLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {homeFooter}
+              </Link>
             </li>
+
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Careers
-              </a>
+              <Link
+                href={skylineLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {skylineFooter}
+              </Link>
             </li>
+
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Brand Center
-              </a>
+              <Link
+                href={airpalLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {airpalFooter}
+              </Link>
             </li>
+
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Blog
-              </a>
+              <Link
+                href={teamLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {teamFooter}
+              </Link>
             </li>
           </ul>
         </div>
 
+        {/* Legal section */}
         <div>
-          <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-            Help center
+          <h2 className="mb-6 text-primary text-sm font-semibold uppercase">
+            {footerTitleThree}
           </h2>
-          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+          <ul className="text-secondary font-medium">
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Discord Server
-              </a>
+              <Link
+                href={termsOfUseLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {termsOfUseFooter}
+              </Link>
             </li>
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Twitter
-              </a>
+              <Link
+                href={privacyPolicyLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {privacyPolicyFooter}
+              </Link>
             </li>
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Facebook
-              </a>
+              <Link
+                href={ethicsLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {ethicsFooter}
+              </Link>
             </li>
+
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Contact Us
-              </a>
+              <Link
+                href={dataProtectionLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {dataProtectionFooter}
+              </Link>
             </li>
           </ul>
         </div>
 
+        {/* Need Help section */}
         <div>
-          <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-            Legal
+          <h2 className="mb-6 text-primary text-sm font-semibold uppercase">
+            {footerTitleTwo}
           </h2>
-          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+          <ul className="text-secondary font-medium">
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Privacy Policy
-              </a>
+              <Link
+                href={contactLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {contactFooter}
+              </Link>
             </li>
+
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Licensing
-              </a>
-            </li>
-            <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Terms &amp; Conditions
-              </a>
+              <Link
+                href={faqLink}
+                className="hover:text-primary hover:duration-150"
+              >
+                {faqFooter}
+              </Link>
             </li>
           </ul>
         </div>
 
+        {/* Follow Us section */}
         <div>
-          <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-            Download
+          <h2 className="mb-6 text-primary text-sm font-semibold uppercase">
+            {footerTitleFour}
           </h2>
-          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+          <ul className="text-secondary font-medium">
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                iOS
+              <a
+                target="_blank"
+                href={crunchbaseURL}
+                className="hover:text-primary hover:duration-150"
+              >
+                {crunchbaseFooter}
               </a>
             </li>
+
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Android
+              <a
+                target="_blank"
+                href={linkedinURL}
+                className="hover:text-primary hover:duration-150"
+              >
+                {linkedinFooter}
               </a>
             </li>
+
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Windows
-              </a>
-            </li>
-            <li className="mb-4">
-              <a href="#" className="hover:underline">
-                MacOS
+              <a
+                target="_blank"
+                href={instagramURL}
+                className="hover:text-primary hover:duration-150"
+              >
+                {instagramFooter}
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-          © 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights
-          Reserved.
+
+      {/* Bottom Note section */}
+      <div className="px-4 py-2 bg-gray-100 flex flex-col md:flex-row md:items-center md:justify-center">
+        <span className="text-sm text-gray-500 sm:text-center block">
+          {copyrightSign} {new Date().getFullYear()}{" "}
+          <a href={averonURL}>{averonTrademarkText}</a>
+          {rightsReservedText}
+        </span>
+      </div>
+      <div className="w-full flex items-center justify-center space-x-2 sm:text-center">
+        <span className="text-sm text-gray-500 dark:text-gray-300">
+          {madeWithText}{" "}
+          <Heart fill="red" stroke="red" className="w-5 h-5 inline" />{" "}
+          {inSwitzerlandText}
         </span>
       </div>
     </div>
--- src/data/content.ts
+++ src/data/content.ts
@@ -1,13 +1,31 @@
+// EXTERNAL LINKS
 export const waitlistURL = "https://www.google.com";
+export const linkedinURL = "https://www.linkedin.com/company/theaveron/";
+export const crunchbaseURL = "https://www.crunchbase.com/organization/averon-6fb2";
+export const instagramURL = "";
+export const averonURL = "https://theaveron.com/";
 
+// INTERNAL ROUTES
 export const homeLink = "/";
 export const skylineLink = "/products/skyline";
 export const airpalLink = "/products/airpal";
 export const teamLink = "/team";
 export const contactLink = "/contact";
+export const termsOfUseLink = "/terms";
+export const faqLink = "/faq";
+export const dataProtectionLink = "/data";
+export const privacyPolicyLink = "/privacy";
+export const ethicsLink = "/ethics";
 
+// CONTENT
 export const joinWaitlist = "Join Waitlist";
+export const madeWithText = "Made with ";
+export const inSwitzerlandText = "In Switzerland";
+export const copyrightSign = "© ";
+export const rightsReservedText = ". All Rights Reserved.";
+export const averonTrademarkText = "Averon™";
 
+// NAVBAR
 export const homeNavbar = "Home";
 export const productsNavbar = "Products";
 export const skylineNavbar = "SkyLine";
@@ -15,11 +33,29 @@ export const airpalNavbar = "AirPal";
 export const teamNavbar = "Our Team";
 export const contactNavbar = "Contact";
 
+// FOOTER TITLE
+export const footerTitleOne = "Company";
+export const footerTitleTwo = "Need Help ?";
+export const footerTitleThree = "Legal";
+export const footerTitleFour = "Follow Us";
+
+// FOOTER 1 DATA
 export const homeFooter = "Home";
-export const productsFooter = "Products";
 export const skylineFooter = "SkyLine";
 export const airpalFooter = "AirPal";
 export const teamFooter = "Our Team";
+
+// FOOTER 3 DATA
 export const contactFooter = "Contact";
-export const termsOfUse = "";
-export const faq = "";
+export const faqFooter = "FAQ";
+
+// FOOTER 2 DATA
+export const termsOfUseFooter = "Terms & Conditions";
+export const privacyPolicyFooter = "Privacy Policy";
+export const dataProtectionFooter = "Data Protection";
+export const ethicsFooter = "AI Ethics";
+
+// FOOTER 4 DATA
+export const linkedinFooter = "LinkedIn";
+export const crunchbaseFooter = "Crunchbase";
+export const instagramFooter = "Instagram";
