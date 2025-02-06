41415a5462b4b04e12c5d7e6dbe72a9603515a44
Alisina <99441567+imalisina@users.noreply.github.com>
2025-02-06 22:15:00 +0100

    skyline features - done

--- src/components/products/skyline/skylineScreen.tsx
+++ src/components/products/skyline/skylineScreen.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import SkyLineHero from "./skylineHero";
 import SkyLineTrailer from "./skylineTrailer";
+import SkyLineFeatures from "./skylineFeatures";
 
 const SkyLineScreen = () => {
   return (
     <div className="mx-auto w-full">
       <SkyLineHero />
       <SkyLineTrailer />
-      <div className="max-w-screen-2xl mx-auto"></div>
+      <div className="max-w-screen-2xl mx-auto">
+        <SkyLineFeatures />
+      </div>
     </div>
   );
 };
